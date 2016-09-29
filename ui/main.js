0b28f5206cbdc4ab2fa93d19e5493ee163dbb45b
reshmi16 <resh.reshmi@gmail.com>
2016-09-29 15:46:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,22 @@
-console.log('Loaded!');
+ var button = document.getelementbyid("counter");
+ var counter = 0;
+ button.onclick = function()
+ { counter = counter + 1;
+   var   request = new XMLHttpRequest();
+   request.onreadystatechange=function()
+   {
+       if (request.readystate ==XMLHttpRequest.done )
+       {
+           if(request.status == 200)
+           {
+               var counter = request.responsetext;
+               var spam = document.getelementbyid("count");
+               span.innerhtml=counter.toString();
+           }
+       }
+   };
+   
+   request.open=getrequest('get','http://reshmi16.imad.hasura-app.io/counter',true);
+   request.send(null);
+ }
+ 
